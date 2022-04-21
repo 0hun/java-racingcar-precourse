0c2c75e928768058d358641e69b9ff71be466ff2
younghun <younghun@paycurx.co.kr>
2022-04-21 20:29:58 +0900

    test (when adding missing tests) : CarsTest 클래스 추가
    test (when adding missing tests) : CarsTest 클래스 추가
    - 자동차 여러대 생성 테스트
    - 자동차 여러대 이동 테스트
    - 자동차 여러대 이동 실패 테스트

---
+++ src/test/java/racingcar/model/CarsTest.java
@@ -0,0 +1,54 @@
+package racingcar.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CarsTest {
+
+    @DisplayName("자동차 여러대 생성 테스트")
+    @Test
+    void carsCreateTest() {
+        //given
+        String[] names = new String[]{"pobi", "woni"};
+        MovableStrategy movableStrategy = () -> RandomNumber.generate() >= 4;
+
+        // when
+        Cars cars = Cars.of(names, movableStrategy);
+
+        // then
+        assertThat(cars.carsSize()).isEqualTo(names.length);
+    }
+
+    @DisplayName("자동차 여러대 이동 테스트")
+    @Test
+    void carsMoveTest() {
+        //given
+        String[] names = new String[]{"pobi", "woni"};
+        MovableStrategy movableStrategy = () -> RandomNumber.generate(4, 9) >= 4;
+        Cars cars = Cars.of(names, movableStrategy);
+
+        // when
+        cars.move();
+
+        // then
+        assertThat(cars.totalCurPosition()).isGreaterThan(0);
+    }
+
+    @DisplayName("자동차 여러대 이동 실패 테스트")
+    @Test
+    void carsMoveTestFail() {
+        //given
+        String[] names = new String[]{"pobi", "woni"};
+        MovableStrategy movableStrategy = () -> RandomNumber.generate(0, 3) >= 4;
+        Cars cars = Cars.of(names, movableStrategy);
+
+        // when
+        cars.move();
+
+        // then
+        assertThat(cars.totalCurPosition()).isEqualTo(0);
+    }
+
+}
